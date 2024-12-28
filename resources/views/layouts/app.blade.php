865c53f7d758945c843a72b05db7e34f38241eb6
Abdi-01 <abdialghi@gmail.com>
2024-12-28 11:27:50 +0700

    #revamp layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,9 @@
 
     <!-- Styles / Scripts -->
     <!-- @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot'))) -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @vite('resources/css/app.css')
+    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
     <!-- @else -->
     <link rel="stylesheet" href="{{ asset('assets/app-C1Xrcf8q.css') }}">
     <!-- @endif -->
