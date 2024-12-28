43379ec59bc2272fdf4251d5c90ccd41b35e4ae9
Abdi-01 <abdialghi@gmail.com>
2024-12-28 14:28:43 +0700

    #revamp manifest dir

---
+++ config/vite.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Vite Manifest Path
+    |--------------------------------------------------------------------------
+    |
+    | Di sini Anda dapat mengatur path ke file manifest.json. 
+    | Secara default: public/build/manifest.json
+    |
+    */
+
+    'manifest_path' => 'dist/manifest.json', // Sesuaikan ke lokasi baru
+];
