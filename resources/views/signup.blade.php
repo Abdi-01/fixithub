876c3c1e4eb8c9733c83cbfb18e4eb269b31090c
Abdi-01 <abdialghi@gmail.com>
2024-12-28 11:42:55 +0700

    #create signup page and route

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Sign Up')
+
+@section('content')
+<div class="py-24">
+
+    <form class="max-w-sm px-10 py-6 bg-gray-800 border-gray-700 rounded-lg shadow-md mx-auto">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Daftarkan Akun</h5>
+        @csrf
+        <x-form-input type="email" label="Email" name="email" placeholder="email@example.com" required />
+        <x-form-input type="password" label="Password" name="password" required />
+        <x-form-input type="password" label="Konfirmasi Password" name="confirm-password" required />
+        <button type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Daftar
+            Sekarang</button>
+    </form>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// #define route for access page
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,3 +23,33 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+// #define route for API call
+Route::post('/submit', function (Request $request) {
+    // #input validation
+    $validated = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required|string|min:8',
+        'confirm-password' => 'required|same:password',
+    ]);
+
+    // #data payload
+    $payload = [
+        'email' => $validated['email'],
+        'password' => $validated['password'],
+    ];
+
+    // #send request to API
+    $response = Http::post('https://kindlyblade-us.backendless.app/api/data/accounts', $payload);
+
+    // #handling response
+    if ($response->successful()) {
+        return back()->with('success', 'Akun berhasil dibuat');
+    } else {
+        return back()->withErrors(['error' => 'Terjadi kesalahan saat membuat akun']);
+    }
+})->name('register.submit');
