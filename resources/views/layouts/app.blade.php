cc09be1b4efe08d522d16448cf84f3d7fb451eda
Abdi-01 <abdialghi@gmail.com>
2024-12-28 11:00:37 +0700

    #revamp layout meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.bunny.net; font-src 'self' fonts.bunny.net;">
+
     <title>@yield('title', 'FixIt Hub')</title>
 
     <!-- Fonts -->
