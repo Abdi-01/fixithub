3b851d729a7c6c5780c03ca1f0b2577c4d1e7bc9
Abdi-01 <abdialghi@gmail.com>
2024-12-28 10:39:31 +0700

    #create layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'FixIt Hub')</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+    <link rel="stylesheet" href="{{ asset('assets/app-C8h9l1WO.css') }}">
+    @endif
+
+</head>
+
+<body class="bg-slate-200 text-gray-900">
+    <x-navbar />
+    <div class="container mx-auto p-4 py-24">
+        @yield('content')
+    </div>
+
+</body>
+
+</html>
