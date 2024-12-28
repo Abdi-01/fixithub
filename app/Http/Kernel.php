e9a13b3df0626b5963594a522001834e4a5e9353
Abdi-01 <abdialghi@gmail.com>
2024-12-28 12:53:22 +0700

    #revamp build

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
 
         // Tambahkan Middleware CSP jika ingin berlaku global
         \App\Http\Middleware\ContentSecurityPolicy::class,
-        \App\Http\Middleware\StaticFilesMiddleware::class,
+        // \App\Http\Middleware\StaticFilesMiddleware::class,
     ];
 
     /**
