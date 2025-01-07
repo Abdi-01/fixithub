75deee52ec4a32e4da3c497658f4d963d1443b3d
Abdi-01 <abdialghi@gmail.com>
2025-01-07 16:06:25 +0700

    revamp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'FixIt Hub')</title>
 
-    <!-- @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-    @vite(['resources/css/app.css'])
-    @else -->
     <link rel="stylesheet" href="{{ \App\Helpers\ViteHelper::asset('resources/css/app.css') }}">
-    <!-- @endif -->
 </head>
 
 <body class="text-gray-900">
