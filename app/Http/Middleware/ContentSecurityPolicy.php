f398be484cd81c37dbbf1b2d54456b1d45c8964e
Abdi-01 <abdialghi@gmail.com>
2025-01-10 14:47:36 +0700

    revamp layout style sync an csp

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -22,7 +22,7 @@ class ContentSecurityPolicy
         $response->headers->set('Content-Security-Policy', "
         default-src 'self'; 
         img-src 'self' https://*; 
-        script-src 'self' 'unsafe-inline' https://cdn.tiny.cloud https://unpkg.com https://cdn.jsdelivr.net; 
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.tiny.cloud https://unpkg.com https://cdn.jsdelivr.net; 
         style-src 'self' 'unsafe-inline' https://cdn.tiny.cloud; 
         connect-src 'self' https:;
     ");
